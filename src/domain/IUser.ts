9eff76fc3413f58570b6541b06965c6ac5822c81
strawbang <dbougouffa@gmail.com>
2022-03-16 12:01:24 +0100

    edit

--- src/domain/IUser.ts
+++
@@ -1,8 +0,0 @@
-export default interface IUser {
-    username: string;
-    password: string;
-    getUsername:() => string;
-    setUsername(username: string);
-    getPassword:() => string;
-    setPassword(password: string);
-}
--- src/domain/User.ts
+++ src/domain/User.ts
@@ -1,30 +1,11 @@
-import IUser from "./IUser";
 import Person from "./Person";
 
-export default class User extends Person implements IUser {
-    username: string;
-    password: string;
+export default class User {
 
-    constructor(firstname: string, lastname: string, username: string, password: string){
-        super(firstname, lastname);
-        this.username = username;
-        this.password = password;
-    }
-
-    getUsername():string{
-        return this.username;
-    }
-
-    setUsername(username: string){
-        this.username = username;
-    }
-
-    getPassword():string{
-        return this.password;
-    }
-
-    setPassword(password: string){
-        this.password = password;
-    }
+    constructor(
+        public readonly username: string,
+        public readonly password: string,
+        public readonly details: Person,
+    ){}
 
 }
